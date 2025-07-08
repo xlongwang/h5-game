5657c75313fe124f2edabd3c23ba8d01c108ae32
along <longwang6@163.com>
2025-07-08 11:09:50 +0800

    feat: 更新 sobre.vue 和 perfil.scss 文件，调整样式以改善页面布局和视觉效果，增加边框尺寸并更新背景图

--- src/views/sobre.vue
+++ src/views/sobre.vue
@@ -10,7 +10,11 @@
     <div class="sobre-page">
         <header-back :title="t('about.title')" :has-back-icon="true" />
         <div class="sobre-page-con">
-            <div class="sobre-page-con-title">{{ t('about.aboutUs') }}</div>
+            <div class="sobre-page-con-title">
+                <div class="sobre-page-con-title-span">
+                    {{ t('about.aboutUs') }}
+                </div>
+            </div>
             <div class="sobre-page-con-text text-[45px] pt-[40px] line-height-[1.5]">
                 {{ t('about.description') }}
             </div>
@@ -31,23 +35,43 @@ const { t } = i18n
 
 <style scoped lang="scss">
 .sobre-page {
-  padding: 0 10px 300px;
+  padding: 0 10px 320px;
 }
 .sobre-page-con {
   width: 945px;
   margin: 0 auto;
-  height: 1707px;
-  background: url("/images/sobre/bg.png") no-repeat center center;
-  background-size: 100% 100%;
+  min-height: 807px;
+  border: 16px solid transparent;
+  border-radius: 32px;
+  border-image: url('/images/sobre/bg.png') 16 fill stretch;
   box-sizing: border-box;
-  padding: 40px 30px 0;
+  padding: 0 0 60px;
 }
 
 .sobre-page-con-text {
   word-break: break-all;
+  padding: 40px 30px 10px
 }
 
 .sobre-page-con-title {
+  height: 115px;
+  font-size: 50px;
+  text-align: left;
+  margin-top: 40px;
+  position: relative;
+    &::after{
+     content: '';
+     display: block;
+     width: 933px;
+     position: absolute;
+     left: -10px;
+     bottom: 0;
+     height: 3px;
+     background: linear-gradient(to right, #573a07 0%, #b4821d 50%, #573a07 100%);
+   }
+}
+
+.sobre-page-con-title-span{
   height: 115px;
   font-size: 50px;
   width: 426px;
@@ -62,7 +86,7 @@ const { t } = i18n
     top: 80px;
     width: 233px;
     height: 11px;
-    background: url("/images/sobre/line.png") no-repeat center center;
+    background: url("./images/sobre/line.png") no-repeat center center;
     background-size: contain;
   }
 }
